7a43f107d49eb18c3966094c3a531a0ed2982bea
huniiali1122@gmail.com <huniiali1122@gmail.com>
2026-02-21 23:19:49 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='flex items-center justify-center h-screen  w-full bg-black text-white'>
-      <h1>hey hunain how are u</h1>
-      
+    <div className='w-full'>
+      <div className='h-screen w-full relative '>
+
+        {/* for outlint back border */}
+        {/* these line cover all Home servies sections */}
+        <div className="Backarea h-full w-full  grid grid-cols-2 ">
+        <div className="h-full w-full border-r border-gray-300"></div>
+                  <div className='h-full w-full ' ></div>
+        </div>
+        <div className="frontarea absolute h-full w-full top-0 left-0 z-99 opacity-0  bg-red-600 "></div>
+
+      </div>
+
+      {/* these line cover all Home servies sections */}
+
+      <div className='h-screen w-full bg-blue-500'></div>
+
+
+
     </div>
   )
 }
--- src/Common/SmoothScrolling.jsx
+++ src/Common/SmoothScrolling.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import LocomotiveScroll from 'locomotive-scroll';
+import React, { useEffect, useRef } from 'react'
 
 const SmoothScrolling = () => {
+    const scrollRef = useRef(null);
+
+    useEffect(() => {
+        const scroll = new LocomotiveScroll({
+          el: scrollRef.current,
+          smooth: true,
+          lerp: 0.08,        // smoothness (0.05 = more smooth)
+          multiplier: 1,     // scroll speed
+          smartphone: {
+            smooth: true,
+          },
+          tablet: {
+            smooth: true,
+          },
+        });
+    
+        return () => {
+          scroll.destroy();
+        };
+      }, []);
   return (
     <div>
       
