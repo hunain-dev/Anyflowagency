ea5ea642b0530a4da68c4aec4be9499ce84030cf
huniiali1122@gmail.com <huniiali1122@gmail.com>
2026-02-21 22:59:56 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center h-screen  w-full bg-black text-white'>
+      <h1>hey hunain how are u</h1>
+      
+    </div>
+  )
+}
+
+export default App
